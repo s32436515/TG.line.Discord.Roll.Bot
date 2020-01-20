036b613573b09c9dd8eb00c491c5aa5ebfc5a616
zeteticl <cougar.l@hotmail.com>
2020-01-20 23:48:33 +0800

    Update z_DDR_darkRollingToGM.js

--- roll/z_DDR_darkRollingToGM.js
+++ roll/z_DDR_darkRollingToGM.js
@@ -18,7 +18,7 @@ try {
         return [/(^[.]drgm$)/ig, ]
     }
     getHelpMessage = function () {
-        return "【暗骰GM功能】.drgm(addgm del show) dr ddr dddr" + "\
+        return "【暗骰GM功能】.drgm(addgm del show myname) dr ddr dddr \
         \n 這是讓你可以私骰GM的功能\
         \n 想成為GM的人先輸入.drgm addgm\
         \n 然後別人DDR 或DDDR (指令)即可以私訊給這位GM\
@@ -27,9 +27,8 @@ try {
         \n 例如輸入 dddr cc 80 鬥毆 \
         \n 就會把結果只私訊GM\
         \n P.S.如果沒立即生效 用.drgm show 刷新一下\
-    \n 輸入.drgm addgm (代名) 即可成為GM,如果想化名一下,\
-        \n 可以在addgm 後輸入一個名字, 暗骰時就會顯示\
-        \n 不輸入就會顯示原名\
+    \n 輸入.drgm addgm  即可成為GM\
+    \n 輸入.drgm myname (名字) 即可以改變自己擲骰時的顯示名字\
     \n 輸入.drgm show 顯示所有GM\
     \n 輸入.drgm del(編號)或all 即可刪除\
     \n 輸入dr  (指令) 私訊自己 \
@@ -76,7 +75,7 @@ try {
                         groupid: groupid,
                         trpgDarkRollingfunction: [{
                             userid: userid,
-                            diyName: mainMsg[2] || "",
+                            //diyName: mainMsg[2] || "",
                             displayname: displayname
                         }]
                         //|| displayname
@@ -91,7 +90,7 @@ try {
                             })
 
                         })
-                        rply.text = '新增成功: ' + (mainMsg[2] || displayname ||
+                        rply.text = '新增成功: ' + (displayname ||
                             "")
                     } else rply.text = '新增失敗. 你已在GM列表'
                 } else {
